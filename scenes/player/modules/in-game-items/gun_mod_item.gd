6f6726fd77e3572d1f6954b4a240a38fae2b1349
Buddy Tynes <hhdrbuddy@gmail.com>
2025-03-13 14:49:45 -0500

    Adds ability to pick up vehicle mods.
    Added the ability to pick up the four vehicle mods available.

---
+++ scenes/player/modules/in-game-items/gun_mod_item.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+@export var mod = preload("res://scenes/player/modules/gun_mod.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	mod = mod.instantiate()
+	animation_player.current_animation = "hover"
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.name == str(multiplayer.get_unique_id()):
+		# return early if we already have the mod.
+		for child in body.get_children():
+			print(child.name)
+			if child.name == mod.name: 
+				print("We already have this mod!")
+				return
+		body.add_child(mod)
+		queue_free()
---
+++ scripts/player/gyro_mod.gd
@@ -0,0 +1,43 @@
+extends Node3D
+
+const STABILIZE_FORCE = 200.0
+const ROTATION_SPEED = 5550.0
+
+@onready var vehicle: Vehicle = $".."
+@onready var ground_distance = $"RayCast3D"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if get_distance_from_ground() == -1: 
+		vehicle.gravity_scale = .25
+		stablize_car(delta) # car in air controll
+	else:
+		vehicle.gravity_scale = 1
+	
+func get_distance_from_ground() -> float:
+	if ground_distance.is_colliding():
+		return ground_distance.get_collision_point().distance_to(ground_distance.global_transform.origin)
+	return -1  # Return -1 if not colliding (indicates not detecting ground)
+
+# needs to be it's own gyro_mod
+func stablize_car(delta):
+	var up_dir = global_transform.basis.y.normalized()
+	var gravity_dir = Vector3.UP
+
+	# Calculate the stabilization force
+	var align_dir = up_dir.cross(gravity_dir).normalized()
+	var align_angle = acos(up_dir.dot(gravity_dir))
+
+	var stabilization_torque = align_dir * align_angle * STABILIZE_FORCE
+	vehicle.apply_torque_impulse(stabilization_torque * delta)
+
+	# Control rotation with right thumbstick
+	var pitch_input = (Input.get_action_strength("ui_right_stick_up") - Input.get_action_strength("ui_right_stick_down")) * -1
+	var roll_input = Input.get_action_strength("ui_right_stick_left") - Input.get_action_strength("ui_right_stick_right")
+	var pitch_torque = global_transform.basis.x * pitch_input * ROTATION_SPEED
+	var roll_torque = global_transform.basis.z * roll_input * ROTATION_SPEED
+	
+	vehicle.axis_lock_angular_x = false
+	vehicle.axis_lock_angular_z = false
+	vehicle.apply_torque_impulse(pitch_torque * delta)
+	vehicle.apply_torque_impulse(roll_torque * delta)
